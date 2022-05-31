88216d5b2a103cdaeb5b8c1c06b3efe26431331c
fahmi amiruddin nafi <fahmi.amiruddin@sirclo.com>
2022-05-31 13:03:36 +0700

    feat([123] Basic NextJS): Routing, Dynamic Route

---
+++ data.js
@@ -0,0 +1,38 @@
+export const news = [
+  {
+    id: 1,
+    title: "berita satu",
+    slug: "berita-satu",
+    content: "Berita satu ini bos"
+  },
+  {
+    id: 2,
+    title: "berita dua",
+    slug: "berita-dua",
+    content: "Berita dua ini bos"
+  }
+];
+
+export const books = [
+  {
+    id: 1,
+    title: "buku satu",
+    slug: "buku-satu",
+    category: "fiction",
+    author: "fahmi"
+  },
+  {
+    id: 2,
+    title: "buku dua",
+    slug: "buku-dua",
+    category: "fiction",
+    author: "amiruddin"
+  },
+  {
+    id: 3,
+    title: "buku tiga",
+    slug: "buku-tiga",
+    category: "novel",
+    author: "nafi"
+  }
+];
---
+++ pages/profile/index.js
@@ -0,0 +1,7 @@
+const Index = () => {
+  return (
+    <div>Index Profile</div>
+  )
+}
+
+export default Index
