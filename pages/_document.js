ec4ef1ef45488ace9dcbd3d4c8133dd73cee5351
fahmi amiruddin nafi <fahmi.amiruddin@sirclo.com>
2022-05-31 15:28:48 +0700

    feat([123456] NextJS Basic): implement next/head & custom document

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function document() {
+  return (
+    <Html>
+      <Head>
+        <title>Basic NextJS</title>
+        <meta name="description" content="Learn Basic NextJS" />
+        <meta name="author" content="Fahmi Amiruddin Nafi"/>
+      </Head>
+      <body>
+        <Main/>
+        <NextScript/>
+      </body>
+    </Html>
+  );
+}
